e9cb8afde5a833328db71f343f77bcc591a3e913
arshia11d <arshia11d@outlook.com>
2022-08-21 21:05:12 +0200

    infra: Install Storybook for demos & visual testing

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+	stories: [
+		"../packages/**/*.stories.mdx",
+		"../packages/**/*.stories.ts"
+	],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials"
+	],
+	framework: "@storybook/web-components",
+	core: {
+		builder: "@storybook/builder-webpack5"
+	},
+	webpackFinal: config => {
+		const [_babel, ...rest] = config.module.rules
+		config.module.rules = [
+			{
+				test: /\.ts?$/,
+				loader: 'ts-loader',
+			},
+			...rest
+		]
+		return config
+	},
+}
