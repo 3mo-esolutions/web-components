303e8a07820663ebaf7c75860f8f02c2cb0dabc6
arshia11d <arshia11d@outlook.com>
2023-02-28 03:18:27 +0100

    feat(VirtualizedScroller): Ability to get the element by index and vice versa
    chore(VirtualizedScroller): No warnings as the scroller already warns the user about intrinsic size
    
    chore(VirtualizedScroller): Expose the item renderer type

--- packages/VirtualizedScroller/VirtualizedScroller.ts
+++ packages/VirtualizedScroller/VirtualizedScroller.ts
@@ -1,9 +1,15 @@
-import { Component, component, css, html, nothing, property } from '@a11d/lit'
+import { Component, component, css, html, nothing, property, query, queryAll } from '@a11d/lit'
 import { LitVirtualizer } from '@lit-labs/virtualizer'
+import { type RenderItemFunction, virtualizerRef } from '@lit-labs/virtualizer/virtualize.js'
 import { Scroller } from '@3mo/scroller'
-import { NoIntrinsicDimensionsController } from '@3mo/no-intrinsic-dimensions-controller'
 
 LitVirtualizer.elementStyles.push(Scroller.scrollbarStyles)
+
+export type GetItemTemplate<T> = RenderItemFunction<T>
+
+export interface VirtualizedElement {
+	scrollIntoView(options?: ScrollIntoViewOptions): void
+}
 
 /**
  * @attr items
@@ -12,7 +18,7 @@ LitVirtualizer.elementStyles.push(Scroller.scrollbarStyles)
 @component('mo-virtualized-scroller')
 export class VirtualizedScroller<T = unknown> extends Component {
 	@property({ type: Array }) items = new Array<T>()
-	@property({ type: Object }) getItemTemplate: LitVirtualizer['renderItem'] = (() => nothing)
+	@property({ type: Object }) getItemTemplate: GetItemTemplate<T> = (() => nothing)
 
 	static override get styles() {
 		return css`
@@ -25,7 +31,34 @@ export class VirtualizedScroller<T = unknown> extends Component {
 		`
 	}
 
-	protected readonly noIntrinsicDimensionsController = new NoIntrinsicDimensionsController(this)
+	@queryAll('lit-virtualizer > *') readonly renderedItems!: Array<HTMLElement>
+
+	@query('lit-virtualizer') protected readonly virtualizerElement!: LitVirtualizer<T>
+
+	private get virtualizer() {
+		return (this.virtualizerElement as any)[virtualizerRef] as {
+			readonly _first: number
+			readonly _last: number
+			element(index: number): VirtualizedElement | undefined
+		}
+	}
+
+	getRenderedElementIndex(element: HTMLElement) {
+		const indexInRenderedElements = this.renderedItems.indexOf(element)
+
+		if (indexInRenderedElements === -1) {
+			return undefined
+		}
+
+		return this.virtualizer._first + indexInRenderedElements
+	}
+
+	getElement(index: number): Element | VirtualizedElement | undefined {
+		const isIndexInRenderedElements = index >= this.virtualizer._first && index <= this.virtualizer._last
+		return isIndexInRenderedElements
+			? this.renderedItems[index - this.virtualizer._first]
+			: this.virtualizer.element(index)
+	}
 
 	protected override get template() {
 		return html`
