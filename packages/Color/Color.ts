b39c303df76ecec8c8b6502cfd195ade259a53ed
arshia11d <arshia11d@outlook.com>
2023-04-14 17:51:38 +0200

    feat(Color): Add the color utility class

---
+++ packages/Color/Color.ts
@@ -0,0 +1,26 @@
+import ColorString from 'color-string'
+
+export class Color {
+	readonly color: ColorString.Color
+
+	constructor(color: string | [r: number, g: number, b: number, a: number]) {
+		this.color = color instanceof Array
+			? color
+			: ColorString.get(color)?.value as ColorString.Color
+	}
+
+	get r() { return this.color[0] }
+	get g() { return this.color[1] }
+	get b() { return this.color[2] }
+	get a() { return this.color[3] }
+
+	get hex() { return ColorString.to.hex(this.color) }
+	get rgb() { return ColorString.to.rgb(this.color) }
+	get rgbPercent() { return ColorString.to.rgb.percent(this.color) }
+	get hsl() { return ColorString.to.hsl(this.color) }
+	get hsv() { return ColorString.to.hwb(this.color) }
+	get keyword() { return ColorString.to.keyword(this.color) }
+
+	toString() { return this.hex }
+	valueOf() { return this.hex }
+}
---
+++ packages/Color/README.md
@@ -0,0 +1,3 @@
+# Color
+
+A color class.
