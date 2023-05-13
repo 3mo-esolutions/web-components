9f392e7b59fceb63cee932465ce8cca8da52394e
a11delavar <a11delavar@outlook.com>
2023-05-13 20:39:06 +0200

    feat(DateTimeValueConstructor): Add the API value constructor for date-time

---
+++ packages/DateTimeValueConstructor/DateTimeValueConstructor.ts
@@ -0,0 +1,13 @@
+import { apiValueConstructor, ApiValueConstructor } from '@a11d/api'
+import '@3mo/date-time'
+
+@apiValueConstructor()
+export class DateValueConstructor implements ApiValueConstructor<DateTime, string> {
+	private static readonly regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/
+
+	shallConstruct = (value: unknown) => typeof value === 'string' && DateValueConstructor.regex.test(value)
+	construct = (text: string) => new DateTime(text)
+
+	shallDeconstruct = (value: unknown) => value instanceof Date
+	deconstruct = (value: DateTime) => value.toISOString()
+}
