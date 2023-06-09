e36cdd1a60ec3f35c409a5412c91ba84d03d203c
a11delavar <a11delavar@outlook.com>
2023-06-09 16:01:46 +0200

    chore(ContextMenu): Change context-menu-items' default height to 40px

--- packages/ContextMenu/ContextMenuItem.ts
+++ packages/ContextMenu/ContextMenuItem.ts
@@ -1,9 +1,19 @@
-import { component } from '@a11d/lit'
+import { component, css } from '@a11d/lit'
 import { NestedMenuItem } from '@3mo/menu'
 
 /** @element mo-context-menu-item */
 @component('mo-context-menu-item')
-export class ContextMenuItem extends NestedMenuItem { }
+export class ContextMenuItem extends NestedMenuItem {
+	static override get styles() {
+		return css`
+			${super.styles}
+
+			:host {
+				height: 40px;
+			}
+		`
+	}
+}
 
 declare global {
 	interface HTMLElementTagNameMap {
