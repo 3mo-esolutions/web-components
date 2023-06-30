f22b1ddcc4881aa482e44fbafb254e044d0cd1c2
a11delavar <a11delavar@outlook.com>
2023-06-30 09:17:23 +0200

    test(Option): Fix a test failing sometimes

--- packages/SelectField/Option.test.ts
+++ packages/SelectField/Option.test.ts
@@ -4,13 +4,15 @@ import { Option } from './Option.js'
 describe('Option', () => {
 	const fixture = new ComponentTestFixture(() => new Option())
 
-	it('should not toggle when clicked in single mode', () => {
+	it('should not toggle when clicked in single mode', async () => {
 		expect(fixture.component.selected).toBeFalse()
 
 		fixture.component.dispatchEvent(new MouseEvent('click'))
+		await fixture.component.updateComplete
 		expect(fixture.component.selected).toBeTrue()
 
 		fixture.component.dispatchEvent(new MouseEvent('click'))
+		await fixture.component.updateComplete
 		expect(fixture.component.selected).toBeTrue()
 	})
 
