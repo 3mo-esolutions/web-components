5b4fb4d0034c8358d7da91aea4f0cd87ed173cd6
a11delavar <a11delavar@outlook.com>
2023-07-04 12:46:58 +0200

    fix(Popover): Fix popover host having height sometimes

--- packages/Popover/PopoverHost.ts
+++ packages/Popover/PopoverHost.ts
@@ -1,10 +1,16 @@
-import { component, html } from '@a11d/lit'
+import { component, css, html } from '@a11d/lit'
 import { NonInertableComponent } from '@a11d/non-inertable-component'
 
 @component('mo-popover-host')
 export class PopoverHost extends NonInertableComponent {
 	static get instance() {
 		return document.querySelector('mo-popover-host') ?? document.body
+	}
+
+	static override get styles() {
+		return css`
+			:host { height: 0px; }
+		`
 	}
 
 	protected override get template() {
