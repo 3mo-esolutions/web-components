da7d78e126954dddb69c903ee1b7dddf1a3ca8c8
a11delavar <a11delavar@outlook.com>
2023-07-07 18:13:47 +0200

    feat(DataGrid): Add built-in deletion column

---
+++ packages/DataGrid/columns/DataGridColumnDeletion.ts
@@ -0,0 +1,41 @@
+import { component, html, event, nothing, property, style } from '@a11d/lit'
+import { Localizer, LanguageCode } from '@3mo/localization'
+import { tooltip } from '@3mo/tooltip'
+import { DataGridColumn } from './DataGridColumn.js'
+
+Localizer.register(LanguageCode.German, {
+	'Delete position': 'Position entfernen'
+})
+
+/**
+ * @element mo-data-grid-column-deletion
+ *
+ * @attr prevent - Prevents the deletion button from being displayed
+ *
+ * @fires delete
+ */
+@component('mo-data-grid-column-deletion')
+export class DataGridColumnDeletion<TData> extends DataGridColumn<TData, never> {
+	@event() readonly delete!: EventDispatcher<TData>
+
+	@property({ type: Boolean }) prevent = false
+
+	override width = '40px'
+	override nonSortable = true
+	override nonEditable = true
+
+	getContentTemplate = (_: never, data?: TData) => this.prevent ? nothing : html`
+		<mo-icon-button icon='delete' ${tooltip('Delete position')}
+			${style({ color: 'var(--mo-color-gray)', height: '40px' })}
+			@click=${() => !data ? void 0 : this.delete.dispatch(data)}
+		></mo-icon-button>
+	`
+
+	getEditContentTemplate = undefined
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'mo-data-grid-column-deletion': DataGridColumnDeletion<unknown>
+	}
+}
