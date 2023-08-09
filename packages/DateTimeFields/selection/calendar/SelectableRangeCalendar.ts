98a263f2af74f9314df0e45ca6ef2f43b0af7c7d
a11delavar <a11delavar@outlook.com>
2023-08-09 13:03:48 +0200

    fix(DateTimeFields): Fix date-time-range field not supporting localization agnostic border radius

--- packages/DateTimeFields/selection/calendar/SelectableRangeCalendar.ts
+++ packages/DateTimeFields/selection/calendar/SelectableRangeCalendar.ts
@@ -20,11 +20,17 @@ export class SelectableRangeCalendar extends SelectableCalendar {
 			}
 
 			.day.selected.first {
-				border-radius: 100px 0 0 100px;
+				border-start-start-radius: 100px;
+				border-start-end-radius: 0;
+				border-end-start-radius: 100px;
+				border-end-end-radius: 0;
 			}
 
 			.day.selected.last {
-				border-radius: 0 100px 100px 0;
+				border-start-start-radius: 0;
+				border-start-end-radius: 100px;
+				border-end-start-radius: 0;
+				border-end-end-radius: 100px;
 			}
 
 			.day.selected.first.last {
