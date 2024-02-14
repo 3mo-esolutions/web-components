6ea464aa52326bee0afb94a765830cce085ecec8
a11delavar <a11delavar@outlook.com>
2024-02-14 13:12:29 +0100

    Add story

--- packages/LanguageField/LanguageField.stories.ts
+++ packages/LanguageField/LanguageField.stories.ts
@@ -1,5 +1,5 @@
 import { story, meta } from '../../.storybook/story.js'
-import { html, ifDefined } from '@a11d/lit'
+import { html, ifDefined, property } from '@a11d/lit'
 import p from './package.json'
 import './index.js'
 import { Language as LanguageBase, LanguageField as LanguageFieldBase } from './index.js'
@@ -19,11 +19,12 @@ export default meta({
 interface Language extends LanguageBase { }
 
 class StoryLanguageField extends LanguageFieldBase<string, Language> {
+	@property({ type: Boolean }) onlyOne = false
 	protected fetch(): Promise<Language[]> {
 		return Promise.resolve([
 			{ id: 1, name: 'English', flagImageSource: 'https://flagsapi.com/GB/flat/64.png' },
-			{ id: 2, name: 'German', flagImageSource: 'https://flagsapi.com/DE/flat/64.png' },
-		])
+			this.onlyOne ? undefined : { id: 2, name: 'German', flagImageSource: 'https://flagsapi.com/DE/flat/64.png' },
+		].filter(Boolean) as Language[])
 	}
 }
 
@@ -86,6 +87,20 @@ export const ModeOverlay = story({
 					value=${value}
 					@change=${(e: CustomEvent<string>) => handleChange(e.detail)}
 				></mo-field-text-area>
+			`}
+		></story-language-field>
+	`
+})
+
+export const WhenOneLanguage = story({
+	render: () => html`
+		<story-language-field onlyOne label='Label' mode='overlay'
+			.fieldTemplate=${(value: string, handleChange: (value: string) => void, label: string, language: Language) => html`
+				<mo-field-text
+					label=${`${label} (${language.name})`}
+					value=${value}
+					@change=${(e: CustomEvent<string>) => handleChange(e.detail)}
+				></mo-field-text>
 			`}
 		></story-language-field>
 	`
