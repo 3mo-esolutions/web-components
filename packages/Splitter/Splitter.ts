d367442a1ea05d03b780cb2cb32af407d1978722
Anton Vorobiov <avorobiov@artelogic.net>
2024-02-21 15:08:49 +0200

    fix(Splitter): Add fallback for TouchEvent

--- packages/Splitter/Splitter.ts
+++ packages/Splitter/Splitter.ts
@@ -80,8 +80,9 @@ export class Splitter extends Component {
 		}
 		const oldTotalSize = this.totalSize
 		const e = this.resizeRequestEvent!
-		const clientX = e instanceof TouchEvent ? e.touches[0]!.clientX : e.clientX
-		const clientY = e instanceof TouchEvent ? e.touches[0]!.clientY : e.clientY
+		// Don't check for instanceof TouchEvent, as Safari doesn't understand it
+		const clientX = 'touches' in e ? e.touches[0]!.clientX : e.clientX
+		const clientY = 'touches' in e ? e.touches[0]!.clientY : e.clientY
 		const { left, top, right, bottom } = resizingItem.getBoundingClientRect()
 
 		const getSize = () => {
