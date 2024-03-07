e203607b9f76f7a885f19d9378ae562d50e97d6a
a11delavar <a11delavar@outlook.com>
2024-03-07 17:43:14 +0100

    Release

--- packages/StandardDialogs/DialogDeletion.ts
+++ packages/StandardDialogs/DialogDeletion.ts
@@ -1,4 +1,4 @@
-import { component, TemplateResult, html, style } from '@a11d/lit'
+import { component, TemplateResult, html, css } from '@a11d/lit'
 import { DialogComponent, NotificationComponent } from '@a11d/lit-application'
 import { LocalStorage } from '@a11d/local-storage'
 import { Localizer } from '@3mo/localization'
@@ -29,17 +29,23 @@ export class DialogDeletion extends DialogComponent<Parameters> {
 		return super.confirm()
 	}
 
+	static override get styles() {
+		return css`
+			mo-dialog {
+				--mo-dialog-default-foreground-content-color: var(--mo-color-foreground-transparent);
+			}
+
+			mo-loading-button {
+				--mo-button-accent-color: var(--mo-color-red);
+			}
+		`
+	}
+
 	protected override get template() {
 		return html`
-			<mo-dialog style='--mo-dialog-default-foreground-content-color: var(--mo-color-foreground-transparent)'
-				heading=${t('Deletion Confirmation')}
-			>
+			<mo-dialog heading=${t('Deletion Confirmation')}>
+				<mo-loading-button type='raised' slot='primaryAction'>${t('Delete')}</mo-loading-button>
 				${this.parameters.content}
-				<mo-button type='outlined' slot='primaryAction'
-					${style({ '--mo-button-accent-color': 'var(--mo-color-red)' })}
-				>
-					${t('Delete')}
-				</mo-button>
 			</mo-dialog>
 		`
 	}
