388df46dcaba5aa84ee9bc4f618522f7898cf433
a11delavar <a11delavar@outlook.com>
2024-04-10 00:29:59 +0200

    feat(DataGrid): Add tooltip support to `eb-data-grid-column-image` via `tooltipSelector` property

--- packages/DataGrid/columns/DataGridColumnImage.ts
+++ packages/DataGrid/columns/DataGridColumnImage.ts
@@ -1,16 +1,38 @@
-import { component, html } from '@a11d/lit'
+import { component, html, property, style } from '@a11d/lit'
 import { DataGridColumn } from './DataGridColumn.js'
+import { tooltip } from '@3mo/tooltip'
 
-/** @element mo-data-grid-column-image */
+/**
+ * @element mo-data-grid-column-image
+ *
+ * @attr tooltipSelector - The data selector of the column to use as a tooltip. If a function is provided, it will be called with the data as an argument.
+ */
 @component('mo-data-grid-column-image')
 export class DataGridColumnImage<TData> extends DataGridColumn<TData, string> {
+	@property() tooltipSelector?: KeyPathOf<TData> | ((data: TData) => string | undefined)
+
 	override nonSortable = true
 	override nonEditable = true
 
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	getContentTemplate(value: string | undefined, _data: TData) {
+	getContentTemplate(value: string | undefined, data: TData) {
+		if (!value) {
+			return html.nothing
+		}
+
+		const tooltipText = !this.tooltipSelector
+			? undefined
+			: typeof this.tooltipSelector === 'function'
+				? this.tooltipSelector(data)
+				: getValueByKeyPath(data, this.tooltipSelector) as string | undefined
+
 		return !value ? html.nothing : html`
-			<img style='vertical-align: middle' src=${value} onload='this.hidden = false' onerror='this.hidden = true'/>
+			<img
+				${tooltipText ? tooltip(tooltipText) : html.nothing}
+				${style({ verticalAlign: 'middle' })}
+				src=${value}
+				onload='this.hidden = false'
+				onerror='this.hidden = true'
+			/>
 		`
 	}
 
