ec2ad6c9562602446d4b23bcde8fbcafa0c17159
a11delavar <a11delavar@outlook.com>
2024-05-04 12:49:02 +0200

    Change FAB positioning

--- packages/DataGrid/DataGrid.ts
+++ packages/DataGrid/DataGrid.ts
@@ -570,7 +570,7 @@ export class DataGrid<TData, TDetailsElement extends Element | undefined = undef
 
 			#fab {
 				position: absolute;
-				top: -72px;
+				bottom: 8px;
 				inset-inline-end: 16px;
 				transition: var(--mo-data-grid-fab-transition, 250ms);
 			}
