6ddac119a831d44d856f37d0704cda4aa492a290
a11delavar <a11delavar@outlook.com>
2024-05-13 14:42:53 +0200

    fix(ScreenSize): Allow `screenSize` directive on element parts

--- packages/ScreenSize/hideOnScreenSize.ts
+++ packages/ScreenSize/hideOnScreenSize.ts
@@ -2,26 +2,27 @@ import { noChange, type PartInfo, PartType, directive } from '@a11d/lit'
 import { DependsOnScreenSizeDirective, type ScreenSizeKey } from './dependsOnScreenSize.js'
 
 export class HideOnScreenSizeDirective extends DependsOnScreenSizeDirective {
-	protected static override readonly supportedPartTypes = [PartType.ELEMENT]
-
-	private element!: HTMLElement
+	private readonly element!: HTMLElement
 	private screenSizes = new Array<ScreenSizeKey>()
 
 	constructor(partInfo: PartInfo) {
 		super(partInfo)
+		if (partInfo.type !== PartType.ELEMENT) {
+			throw new Error('Directive not supported on this part type.')
+		}
 		// @ts-expect-error - partInfo.element is not defined in the type
 		this.element = partInfo.element as HTMLElement
-		this.handleChange()
+		this.handleEvent()
 	}
 
 	// @ts-expect-error - Overriding the signature
 	override render(...screenSizes: Array<ScreenSizeKey>) {
 		this.screenSizes = screenSizes
-		this.handleChange()
+		this.handleEvent()
 		return noChange
 	}
 
-	protected override handleChange() {
+	override handleEvent() {
 		const matches = this.screenSizes.some(screenSize => DependsOnScreenSizeDirective.media[screenSize]?.matches ?? false)
 		this.element.style.display = matches ? 'none' : ''
 	}
