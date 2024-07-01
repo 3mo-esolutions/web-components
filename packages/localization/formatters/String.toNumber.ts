e9e07421f11bb57748df6238a79bc636932af35f
a11delavar <a11delavar@outlook.com>
2024-07-01 11:24:54 +0200

    perf(localization): Improve performance of String.toNumber method by caching the reverse-engineering regex objects

--- packages/localization/formatters/String.toNumber.ts
+++ packages/localization/formatters/String.toNumber.ts
@@ -1,15 +1,26 @@
 import { Localizer } from '../Localizer.js'
 import { type LanguageCode } from '../LanguageCode.js'
 
+const separatorRegexByLanguage = new Map<LanguageCode, { readonly thousandRegex: RegExp, readonly decimalRegex: RegExp }>()
+const spaceRegex = / /g
+
 String.prototype.toNumber = function (this: string, language = Localizer.currentLanguage) {
-	const numberString = this.replace(/ /g, '')
+	if (!separatorRegexByLanguage.has(language)) {
+		const thousandSeparator = Intl.NumberFormat(language).formatToParts(1000).find(p => p.type === 'group')?.value ?? ''
+		const thousandRegex = new RegExp(`\\${thousandSeparator}`, 'g')
 
-	const thousandSeparator = Intl.NumberFormat(language).formatToParts(1000).find(p => p.type === 'group')?.value ?? ''
-	const decimalSeparator = Intl.NumberFormat(language).formatToParts(1.1).find(p => p.type === 'decimal')?.value ?? ''
+		const decimalSeparator = Intl.NumberFormat(language).formatToParts(1.1).find(p => p.type === 'decimal')?.value ?? ''
+		const decimalRegex = new RegExp(`\\${decimalSeparator}`)
 
+		separatorRegexByLanguage.set(language, { thousandRegex, decimalRegex })
+	}
+
+	const { thousandRegex, decimalRegex } = separatorRegexByLanguage.get(language)!
+
+	const numberString = this.replace(spaceRegex, '')
 	const number = parseFloat(numberString
-		.replace(new RegExp(`\\${thousandSeparator}`, 'g'), '')
-		.replace(new RegExp(`\\${decimalSeparator}`), '.')
+		.replace(thousandRegex, '')
+		.replace(decimalRegex, '.')
 	)
 
 	return Number.isNaN(number) ? undefined : number
