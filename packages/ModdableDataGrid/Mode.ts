d10264755c0e9ba0dd38efca43c7f31f01dd401c
Anton Vorobiov <avorobiov@artelogic.net>
2024-08-14 11:04:28 +0300

    fix

--- packages/ModdableDataGrid/Mode.ts
+++ packages/ModdableDataGrid/Mode.ts
@@ -1,6 +1,10 @@
 import { type DataGridColumn, type DataGridPagination, type DataGridSorting } from '@3mo/data-grid'
 import { type FetchableDataGridParametersType } from '@3mo/fetchable-data-grid'
 
+const generateId = () => {
+	return Math.floor(Math.random() * Date.now())
+}
+
 export class Mode<TData, TDataFetcherParameters extends FetchableDataGridParametersType> {
 	name!: string
 	id? = generateId()
@@ -10,13 +14,7 @@ export class Mode<TData, TDataFetcherParameters extends FetchableDataGridParamet
 	pagination?: DataGridPagination
 	parameters?: TDataFetcherParameters
 
-	constructor(mode?: Partial<Mode<TData, TDataFetcherParameters>>) {
-		if (mode) {
-			Object.assign(this, mode)
-		}
+	constructor(origin?: Partial<Mode<TData, TDataFetcherParameters>>) {
+		Object.assign(this, origin)
 	}
-}
-
-function generateId() {
-	return Math.floor(Math.random() * Date.now())
 }
--- packages/ModdableDataGrid/ModeRepository.ts
+++ packages/ModdableDataGrid/ModeRepository.ts
@@ -7,45 +7,71 @@ import { objectEquals } from './ObjectExtensions.js'
 
 export class ModeRepository<TData, TDataFetcherParameters extends FetchableDataGridParametersType> extends LocalStorage<Array<Mode<TData, TDataFetcherParameters>>> {
 	private _defaultMode?: Required<Mode<TData, TDataFetcherParameters>>
-	get defaultMode() { return this._defaultMode! }
 
-	constructor(private readonly dataGrid: ModdableDataGrid<TData, TDataFetcherParameters>) {
-		super(`ModdableDataGrid.${dataGrid.tagName.toLowerCase()}.Modes`, [], (_key: string, value: any) => {
-			return (DateTime.isoRegularExpression.test(value))
-				? new Date(value)
-				: value
-		})
+	get defaultMode() {
+		return this._defaultMode!
 	}
 
-	updateDefaultIfNeeded() {
-		if (!this._defaultMode) {
-			this._defaultMode = this.currentMode
+	constructor(
+		private readonly dataGrid: ModdableDataGrid<TData, TDataFetcherParameters>
+	) {
+		const reviver = (_: string, value: any) => {
+			if (typeof value === 'object' && value.start || value.end) {
+				return new DateTimeRange(value.start, value.end)
+			}
+			if (DateTime.isoRegularExpression.test(value)) {
+				return new Date(value)
+			}
+			return value
 		}
+
+		super(`ModdableDataGrid.${dataGrid.tagName.toLowerCase()}.Modes`, [], reviver)
 	}
 
-	override get value() { return super.value.map(mode => new Mode<TData, TDataFetcherParameters>(mode)) }
+	override get value() {
+		return super.value.map(mode => new Mode<TData, TDataFetcherParameters>(mode))
+	}
+
 	override set value(value) {
 		super.value = value
 		this.dataGrid.requestUpdate()
 	}
 
-	get(id: number) {
+	get currentMode() {
+		if (this.dataGrid.mode) {
+			return this.dataGrid.mode as Required<Mode<TData, TDataFetcherParameters>>
+		}
+		return {
+			sorting: this.dataGrid.sorting,
+			parameters: this.dataGrid.parameters ?? undefined,
+			columns: this.dataGrid.columns,
+			pagination: this.dataGrid.pagination,
+		} as Required<Mode<TData, TDataFetcherParameters>>
+	}
+
+	updateDefaultIfNeeded = () => {
+		if (!this._defaultMode) {
+			this._defaultMode = this.currentMode
+		}
+	}
+
+	get = (id: number) => {
 		return this.value.find(mode => mode.id === id)
 	}
 
-	getAll() {
+	getAll = () => {
 		return super.value
 	}
 
-	getUnarchived() {
-		return this.getAll().filter(mode => mode.isArchived === false)
+	getUnarchived = () => {
+		return this.getAll().filter(mode => !mode.isArchived)
 	}
 
-	getArchived() {
-		return this.getAll().filter(mode => mode.isArchived === true)
+	getArchived = () => {
+		return this.getAll().filter(mode => mode.isArchived)
 	}
 
-	isSelected(mode: Mode<TData, TDataFetcherParameters>) {
+	isSelected = (mode: Mode<TData, TDataFetcherParameters>) => {
 		return this.dataGrid.mode?.id === mode.id
 	}
 
@@ -55,18 +81,8 @@ export class ModeRepository<TData, TDataFetcherParameters extends FetchableDataG
 			|| objectEquals(this.dataGrid.mode, this.currentMode)
 	}
 
-	get currentMode() {
-		return {
-			...(this.dataGrid.mode ?? {}),
-			sorting: this.dataGrid.sorting,
-			parameters: this.dataGrid.parameters ?? undefined,
-			// sync?
-			columns: this.dataGrid.mode?.columns ?? this.dataGrid.columns,
-			pagination: this.dataGrid.pagination,
-		} as Required<Mode<TData, TDataFetcherParameters>>
-	}
 
-	save(mode: Mode<TData, TDataFetcherParameters> = this.currentMode) {
+	save = (mode: Mode<TData, TDataFetcherParameters> = this.currentMode) => {
 		const existingMode = !mode.id ? undefined : this.get(mode.id)
 
 		mode.columns = mode.columns?.map(c => ({
@@ -94,19 +110,19 @@ export class ModeRepository<TData, TDataFetcherParameters extends FetchableDataG
 		}
 	}
 
-	remove(mode: Mode<TData, TDataFetcherParameters>) {
+	remove = (mode: Mode<TData, TDataFetcherParameters>) => {
 		this.value = this.value.filter(m => m.id !== mode.id)
 		if (this.dataGrid.mode?.id === mode.id) {
 			this.dataGrid.mode = undefined
 		}
 	}
 
-	archive(mode: Mode<TData, TDataFetcherParameters>) {
+	archive = (mode: Mode<TData, TDataFetcherParameters>) => {
 		mode.isArchived = true
 		this.save(mode)
 	}
 
-	unarchive(mode: Mode<TData, TDataFetcherParameters>) {
+	unarchive = (mode: Mode<TData, TDataFetcherParameters>) => {
 		mode.isArchived = false
 		this.save(mode)
 	}
