911e3ddd543c802889dea9e18fcfc873e4887145
Anton Vorobiov <avorobiov@artelogic.net>
2024-09-02 16:35:06 +0300

    fix: Add background color for row details container

--- packages/DataGrid/rows/DataGridDefaultRow.ts
+++ packages/DataGrid/rows/DataGridDefaultRow.ts
@@ -29,6 +29,10 @@ export class DataGridDefaultRow<TData, TDetailsElement extends Element | undefin
 				grid-column: -1 / 1;
 			}
 
+			:host(:not([has-sub-data])) #detailsContainer {
+				background-color: var(--mo-color-transparent-gray-1);
+			}
+
       :host(:not([has-sub-data])) #detailsContainer > * {
         margin: 15px 60px !important;
 				width: calc(100% - 120px);
