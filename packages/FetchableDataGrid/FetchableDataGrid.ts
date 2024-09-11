bdd6ad4a5adb772bc94081af6073b3f54ac22c60
Anton Vorobiov <avorobiov@artelogic.net>
2024-09-11 17:37:21 +0300

    fix: Add ts-ignore for sidePanelTemplate visibility

--- packages/FetchableDataGrid/FetchableDataGrid.ts
+++ packages/FetchableDataGrid/FetchableDataGrid.ts
@@ -40,6 +40,7 @@ Localizer.dictionaries.add('de', {
  * @fires dataFetch
  */
 @component('mo-fetchable-data-grid')
+// @ts-ignore `sidePanelTemplate` is private in `DataGrid`
 export class FetchableDataGrid<TData, TDataFetcherParameters extends FetchableDataGridParametersType = Record<string, never>, TDetailsElement extends Element | undefined = undefined> extends DataGrid<TData, TDetailsElement> {
 	@event() readonly parametersChange!: EventDispatcher<TDataFetcherParameters | undefined>
 	@event() readonly dataFetch!: EventDispatcher<Result<TData>>
@@ -256,7 +257,7 @@ export class FetchableDataGrid<TData, TDataFetcherParameters extends FetchableDa
 		`
 	}
 
-	protected override get sidePanelTemplate() {
+	override get sidePanelTemplate() {
 		return html`
 			<mo-data-grid-side-panel
 				.dataGrid=${this as any}
