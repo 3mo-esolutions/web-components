e99b74e8bba811ed2612fdba0d76399597623afb
a11delavar <a11delavar@outlook.com>
2024-10-10 15:29:59 +0200

    test: More precise date-time tests

--- packages/DateTime/expectDateTimesEquals.test.ts
+++ packages/DateTime/expectDateTimesEquals.test.ts
@@ -1,5 +1,7 @@
 export const expectDateTimesEquals = (actual: DateTime | undefined, expected: DateTime | undefined) => {
-	const actualRounded = actual?.round('minutes')
-	const expectedRounded = expected?.round('minutes')
-	expect(actualRounded).withContext(`actual: ${actual} and expected: ${expected} are not equal`).toEqual(expectedRounded)
+	const actualRounded = actual?.valueOf() ?? 0
+	const expectedRounded = expected?.valueOf() ?? 0
+	expect(Math.abs(expectedRounded - actualRounded))
+		.withContext(`actual [${actual}] and expected [${expected}] are not equal`)
+		.toBeLessThan(1000)
 }
