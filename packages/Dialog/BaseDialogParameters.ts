5ade9eab0e8fa07543f9690da63570b8f6ca53c8
a11delavar <a11delavar@outlook.com>
2024-10-22 16:11:36 +0200

    feat(StandardDialogs): Default confirmation text for deletion dialogs
    feat(StandardDialogs): Ability to pass the label of the deleting entity to the deletion dialogs for enhanced confirmation

--- packages/Dialog/BaseDialogParameters.ts
+++ packages/Dialog/BaseDialogParameters.ts
@@ -1,8 +1,8 @@
-import { type TemplateResult } from '@a11d/lit'
+import { type HTMLTemplateResult } from '@a11d/lit'
 import type { DialogSize } from './Dialog.js'
 import { type DialogComponent } from '@a11d/lit-application'
 
-export type DialogContent<Dialog extends DialogComponent<any, any>> = undefined | string | TemplateResult | ((this: Dialog) => string | TemplateResult)
+export type DialogContent<Dialog extends DialogComponent<any, any>> = undefined | string | HTMLTemplateResult | ((this: Dialog) => string | HTMLTemplateResult)
 
 export interface BaseDialogParameters<Dialog extends DialogComponent<any, any>> {
 	readonly heading: string
@@ -12,6 +12,6 @@ export interface BaseDialogParameters<Dialog extends DialogComponent<any, any>> 
 	readonly size?: DialogSize
 }
 
-export const getContentTemplate = <Dialog extends DialogComponent<any, any>>(context: Dialog, content: DialogContent<Dialog>) => {
-	return typeof content === 'function' ? content.call(context) : content ?? ''
+export const getContentTemplate = <Dialog extends DialogComponent<any, any>>(context: Dialog, content: DialogContent<Dialog>, defaultValue: HTMLTemplateResult | string = '') => {
+	return typeof content === 'function' ? content.call(context) : content ?? defaultValue
 }
