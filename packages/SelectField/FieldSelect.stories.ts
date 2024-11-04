372593e02e638936cfc1ef5e0f709f806b8aa6ac
a11delavar <a11delavar@outlook.com>
2024-11-04 23:46:11 +0100

    Migrate stories

--- packages/SelectField/FieldSelect.stories.ts
+++ packages/SelectField/FieldSelect.stories.ts
@@ -21,7 +21,7 @@ export const Select: StoryObj = {
 			<mo-field-select label='Countries' ?searchable=${searchable} ?multiple=${multiple} default=${defaultText}>
 				${countries.map(country => html`
 					<mo-option value=${country.code} .data=${country}>
-						<img width='25px' src=${`https://flagcdn.com/h40/${country.code.toLowerCase()}.png`}>
+						<img slot='start' width='25px' src=${`https://flagcdn.com/h40/${country.code.toLowerCase()}.png`}>
 						${country.label}
 					</mo-option>
 				`)}
@@ -75,7 +75,7 @@ class StoryFreeInputEvents extends Component {
 				>
 					${countries.map(country => html`
 						<mo-option value=${country.code} .data=${country}>
-							<img width='25px' src=${`https://flagcdn.com/h40/${country.code.toLowerCase()}.png`}>
+							<img slot='start' width='25px' src=${`https://flagcdn.com/h40/${country.code.toLowerCase()}.png`}>
 							${country.label}
 						</mo-option>
 					`)}
@@ -120,7 +120,7 @@ export const PreSelectedValue: StoryObj = {
 			<mo-field-select label='Countries' ?searchable=${searchable} ?multiple=${multiple} default=${defaultText} .value=${multiple ? [] : 'DE'}>
 				${countries.map(country => html`
 					<mo-option value=${country.code} .data=${country}>
-						<img width='25px' src=${`https://flagcdn.com/h40/${country.code.toLowerCase()}.png`}>
+						<img slot='start' width='25px' src=${`https://flagcdn.com/h40/${country.code.toLowerCase()}.png`}>
 						${country.label}
 					</mo-option>
 				`)}
