3cbf08137858151d527baf56d21388d96c71bb9e
a11delavar <a11delavar@outlook.com>
2024-11-05 14:06:34 +0100

    feat(SelectField): Expose `menu` and `list` CSS parts

--- packages/SelectField/FieldSelect.ts
+++ packages/SelectField/FieldSelect.ts
@@ -25,6 +25,8 @@ import { Option } from './Option.js'
  *
  * @csspart input - The input element.
  * @csspart dropDownIcon - The dropdown icon.
+ * @csspart menu - The menu consisting of list of options.
+ * @csspart list - The list of options.
  *
  * @i18n "No results"
  *
@@ -106,6 +108,7 @@ export class FieldSelect<T> extends FieldComponent<Value> {
 			:host {
 				display: flex;
 				flex-flow: column;
+				--_grid-column-full-span-in-case: 1 / -1;
 			}
 
 			input {
@@ -135,18 +138,18 @@ export class FieldSelect<T> extends FieldComponent<Value> {
 
 			mo-list-item {
 				min-height: 40px;
+				grid-column: var(--_grid-column-full-span-in-case);
 			}
 
-			slot:not([name]) {
-				display: flex;
-				flex-direction: column;
-				align-items: stretch;
+			mo-line {
+				grid-column: var(--_grid-column-full-span-in-case);
 			}
 
 			#no-options-hint {
 				display: none;
 				padding: 10px;
 				color: var(--mo-color-gray);
+				grid-column: var(--_grid-column-full-span-in-case);
 			}
 
 			:host([data-show-no-options-hint]) #no-options-hint {
@@ -221,7 +224,7 @@ export class FieldSelect<T> extends FieldComponent<Value> {
 
 	protected get menuTemplate() {
 		return html`
-			<mo-menu
+			<mo-menu part='menu' exportparts='list'
 				fixed
 				target='field'
 				selectionMode=${this.multiple ? 'multiple' : 'single'}
