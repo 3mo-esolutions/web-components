32c4430d85e78a21d63f430e265fb898b2d15f91
a11delavar <a11delavar@outlook.com>
2025-02-10 21:34:27 +0100

    chore(EntityDialog): Infer the entity label only via `toString()` to prevent assumptions about the consumer's domain logic

--- packages/EntityDialog/getEntityLabel.ts
+++ packages/EntityDialog/getEntityLabel.ts
@@ -1,10 +1,7 @@
-import { label } from '@a11d/metadata'
 import { Localizer } from '@3mo/localization'
 
 Localizer.dictionaries.add('de', { 'Entity': 'Eintrag' })
 
 export function getEntityLabel<T extends object>(entity: T) {
-	const _label = label.get(entity.constructor as Constructor<T>) || t('Entity')
-	const _string = entity.toString === Object.prototype.toString ? undefined : (entity.toString() || undefined)
-	return [_label, _string ? `"${_string}"` : undefined].filter(Boolean).join(' ')
+	return entity.toString === Object.prototype.toString ? t('Entity') : entity.toString() || t('Entity')
 }
