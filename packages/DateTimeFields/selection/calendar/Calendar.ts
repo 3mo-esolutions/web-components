013097334684b48ccb3f5101bfa509a1598cfe3e
a11delavar <a11delavar@outlook.com>
2025-05-30 14:14:00 +0200

    File restructure

--- packages/DateTimeFields/selection/calendar/Calendar.ts
+++ packages/DateTimeFields/selection/calendar/Calendar.ts
@@ -1,96 +1,8 @@
-import { Component, css, component, html, property, classMap, type ClassInfo, event, Controller, repeat } from '@a11d/lit'
+import { Component, css, component, html, property, classMap, type ClassInfo, event, repeat } from '@a11d/lit'
 import '@a11d/array.prototype.group'
-import { observeIntersection } from '@3mo/intersection-observer'
-import { MemoizeExpiring as memoizeExpiring } from 'typescript-memoize'
+import { CalendarDatesController } from './CalendarDatesController.js'
 
-class CalendarDatesController extends Controller {
-	@memoizeExpiring(60_000)
-	static get today() { return new DateTime().dayStart }
-
-	private static *generate(start: DateTime, count: number, step = 'days') {
-		for (let i = 0; i < count; i++) {
-			yield start.add({ [step]: i })
-		}
-	}
-
-	private static _sampleWeek = new Array<DateTime>()
-	static get sampleWeek() { return this._sampleWeek as ReadonlyArray<DateTime> }
-
-	private static generateWeek() {
-		const sample = [...CalendarDatesController.generate(CalendarDatesController.today, CalendarDatesController.today.daysInWeek * 2)]
-		const indexOfFirstWeekStart = sample.findIndex(d => d.dayOfWeek === 1)
-		const daysInWeek = sample[0]!.daysInWeek
-		CalendarDatesController._sampleWeek = sample.slice(indexOfFirstWeekStart, indexOfFirstWeekStart + daysInWeek).map(d => d.dayStart)
-	}
-
-	static {
-		CalendarDatesController.generateWeek()
-	}
-
-	constructor(override readonly host: Calendar) {
-		super(host)
-		this.navigationDate = CalendarDatesController.today
-	}
-
-	private _days = new Array<DateTime>()
-	private _months = new Array<DateTime>()
-	private _years = new Array<DateTime>()
-
-	get data() {
-		switch (this.host.view) {
-			case 'month':
-				return this._months
-			case 'year':
-				return this._years
-			default:
-				return this._days
-		}
-	}
-
-	private _navigationDate!: DateTime
-	get navigationDate() { return this._navigationDate }
-	set navigationDate(value) {
-		let changed = false
-
-		const daysOffset = 80
-		if (this.host.view === 'day' && (!this._days.length || value.isBefore(this._days.at(daysOffset)!) || value.isAfter(this._days.at(-daysOffset)!))) {
-			this._days = [...CalendarDatesController.generate(
-				value.yearStart.add({ years: -1 }),
-				value.daysInYear * 3,
-				'days',
-			)]
-			changed = true
-		}
-
-		const monthsOffset = 25
-		if (this.host.view === 'month' && (!this._months.length || value.isBefore(this._months.at(monthsOffset)!) || value.isAfter(this._months.at(-monthsOffset)!))) {
-			this._months = [...CalendarDatesController.generate(
-				value.yearStart.add({ years: -10 }),
-				value.monthsInYear * 20,
-				'months',
-			)]
-			changed = true
-		}
-
-		const yearsOffset = 10
-		if (this.host.view === 'year' && (!this._years.length || value.isBefore(this._years.at(yearsOffset)!) || value.isAfter(this._years.at(-yearsOffset)!))) {
-			this._years = [...CalendarDatesController.generate(
-				value.yearStart.add({ years: -100 }),
-				200,
-				'years',
-			)]
-			changed = true
-		}
-
-		if (changed) {
-			this.host.requestUpdate()
-		}
-
-		this._navigationDate = value.dayStart
-	}
-}
-
-type CalendarView = 'year' | 'month' | 'day'
+export type CalendarView = 'year' | 'month' | 'day'
 
 /**
  * @fires dayClick - Dispatched when a day is clicked, with the clicked date as detail.
@@ -103,12 +15,12 @@ export class Calendar extends Component {
 
 	@property() view: CalendarView = 'day'
 
-	private readonly daysController = new CalendarDatesController(this)
+	private readonly datesController = new CalendarDatesController(this)
 
-	get navigationDate() { return this.daysController.navigationDate }
+	get navigationDate() { return this.datesController.navigationDate }
 
 	async setNavigatingValue(date: DateTime, behavior: 'instant' | 'smooth' = 'instant') {
-		this.daysController.navigationDate = date
+		this.datesController.navigationDate = date
 		await this.updateComplete
 		await new Promise(r => setTimeout(r, 0))
 		this.scrollToNavigatingItem(behavior)
@@ -273,7 +185,7 @@ export class Calendar extends Component {
 	protected override get template() {
 		return html`
 			<mo-scroller data-view=${this.view}>
-				${repeat(this.daysController.data, d => d.toString(), d => html`
+				${repeat(this.datesController.data, d => d.toString(), d => html`
 					${this.getYearTemplate(d)}
 					${this.getMonthTemplate(d)}
 				`)}
@@ -281,19 +193,11 @@ export class Calendar extends Component {
 		`
 	}
 
-	private observerIntersectionNavigation(date: DateTime, ...views: CalendarView[]) {
-		return !views.includes(this.view) ? html.nothing : observeIntersection(data => {
-			if (data.some(entry => entry.isIntersecting)) {
-				this.daysController.navigationDate = date
-			}
-		})
-	}
-
 	private getYearTemplate(date: DateTime) {
 		return date.dayOfYear !== 1 ? html.nothing : html`
 			<div class='year' role='button'
 				data-view=${this.view}
-				${this.observerIntersectionNavigation(date, 'month', 'year')}
+				${this.datesController.observerIntersectionNavigation(date, 'month', 'year')}
 				@click=${() => this.setView(date, this.view === 'year' ? 'month' : 'year')}
 			>
 				${date.format({ year: 'numeric' })}
@@ -326,14 +230,14 @@ export class Calendar extends Component {
 				<div class='month' role='button'
 					?data-navigating=${this.navigationDate.year === date.year && this.navigationDate.month === date.month}
 					data-view=${this.view}
-					${this.observerIntersectionNavigation(date, 'day')}
+					${this.datesController.observerIntersectionNavigation(date, 'day')}
 					@click=${() => this.setView(date, this.view === 'day' ? 'month' : 'day')}
 				>
 					${this.view === 'day' ? date.format({ year: 'numeric', month: 'long' }) : date.format({ month: 'long' })}
 				</div>
 				<mo-grid autoRows='var(--mo-calendar-item-size)' columns=${this.view === 'day' ? this.columns : 'auto'}>
 					${this.view !== 'day' ? html.nothing : Calendar.weekDaysTemplate}
-					${this.daysController.data.filter(d => d.year === date.year && d.month === date.month).map(day => this.getDayTemplate(day))}
+					${this.datesController.data.filter(d => d.year === date.year && d.month === date.month).map(day => this.getDayTemplate(day))}
 				</mo-grid>
 			</mo-flex>
 		`
---
+++ packages/DateTimeFields/selection/calendar/CalendarDatesController.ts
@@ -0,0 +1,99 @@
+import { Controller, html, type DirectiveResult } from '@a11d/lit'
+import { observeIntersection } from '@3mo/intersection-observer'
+import { MemoizeExpiring as memoizeExpiring } from 'typescript-memoize'
+import type { Calendar, CalendarView } from './Calendar.js'
+
+export class CalendarDatesController extends Controller {
+	@memoizeExpiring(60_000)
+	static get today() { return new DateTime().dayStart }
+
+	private static *generate(start: DateTime, count: number, step = 'days') {
+		for (let i = 0; i < count; i++) {
+			yield start.add({ [step]: i })
+		}
+	}
+
+	private static _sampleWeek = new Array<DateTime>()
+	static get sampleWeek() { return this._sampleWeek as ReadonlyArray<DateTime> }
+
+	private static generateWeek() {
+		const sample = [...CalendarDatesController.generate(CalendarDatesController.today, CalendarDatesController.today.daysInWeek * 2)]
+		const indexOfFirstWeekStart = sample.findIndex(d => d.dayOfWeek === 1)
+		const daysInWeek = sample[0]!.daysInWeek
+		CalendarDatesController._sampleWeek = sample.slice(indexOfFirstWeekStart, indexOfFirstWeekStart + daysInWeek).map(d => d.dayStart)
+	}
+
+	static {
+		CalendarDatesController.generateWeek()
+	}
+
+	observerIntersectionNavigation(date: DateTime, ...views: CalendarView[]) {
+		return !views.includes(this.host.view) ? html.nothing : observeIntersection(data => {
+			if (data.some(entry => entry.isIntersecting)) {
+				this.navigationDate = date
+			}
+		}) as DirectiveResult<any>
+	}
+
+	constructor(override readonly host: Calendar) {
+		super(host)
+		this.navigationDate = CalendarDatesController.today
+	}
+
+	private days = new Array<DateTime>()
+	private months = new Array<DateTime>()
+	private years = new Array<DateTime>()
+
+	get data() {
+		switch (this.host.view) {
+			case 'month':
+				return this.months
+			case 'year':
+				return this.years
+			default:
+				return this.days
+		}
+	}
+
+	private _navigationDate!: DateTime
+	get navigationDate() { return this._navigationDate }
+	set navigationDate(value) {
+		let changed = false
+
+		const daysOffset = 80
+		if (this.host.view === 'day' && (!this.days.length || value.isBefore(this.days.at(daysOffset)!) || value.isAfter(this.days.at(-daysOffset)!))) {
+			this.days = [...CalendarDatesController.generate(
+				value.yearStart.add({ years: -1 }),
+				value.daysInYear * 3,
+				'days',
+			)]
+			changed = true
+		}
+
+		const monthsOffset = 25
+		if (this.host.view === 'month' && (!this.months.length || value.isBefore(this.months.at(monthsOffset)!) || value.isAfter(this.months.at(-monthsOffset)!))) {
+			this.months = [...CalendarDatesController.generate(
+				value.yearStart.add({ years: -10 }),
+				value.monthsInYear * 20,
+				'months',
+			)]
+			changed = true
+		}
+
+		const yearsOffset = 10
+		if (this.host.view === 'year' && (!this.years.length || value.isBefore(this.years.at(yearsOffset)!) || value.isAfter(this.years.at(-yearsOffset)!))) {
+			this.years = [...CalendarDatesController.generate(
+				value.yearStart.add({ years: -100 }),
+				200,
+				'years',
+			)]
+			changed = true
+		}
+
+		if (changed) {
+			this.host.requestUpdate()
+		}
+
+		this._navigationDate = value.dayStart
+	}
+}
