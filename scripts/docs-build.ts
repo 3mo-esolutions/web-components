72f5eb27fe21887612b1d203ab892d4da61a5cb3
a11delavar <a11delavar@outlook.com>
2025-09-24 21:56:26 +0200

    chore: Target ES2022 for upcoming releases

--- scripts/docs-build.ts
+++ scripts/docs-build.ts
@@ -2,6 +2,6 @@ import { run } from './util/index.ts'
 
 await Promise.all([
 	run('npm run analyze', undefined, true),
-	run('node ./scripts/changelog.mjs'),
+	run('node ./scripts/changelog.ts'),
 ])
 await run('storybook build -o docs-dist', undefined, true)
