ce52e444fda79198867a2baebca4e86449338a28
a11delavar <a11delavar@outlook.com>
2025-12-30 18:46:03 +0100

    feat(DataGrid): Add `formatOptions` property to `mo-data-grid-column-number`

--- packages/DataGrid/columns/number/DataGridColumnNumber.ts
+++ packages/DataGrid/columns/number/DataGridColumnNumber.ts
@@ -1,12 +1,15 @@
-import { component, html, ifDefined, style } from '@a11d/lit'
+import { component, html, ifDefined, property, style } from '@a11d/lit'
+import { hasChanged } from '@a11d/equals'
 import { DataGridColumnNumberBase } from './DataGridColumnNumberBase.js'
 
 /** @element mo-data-grid-column-number */
 @component('mo-data-grid-column-number')
 export class DataGridColumnNumber<TData> extends DataGridColumnNumberBase<TData> {
+	@property({ type: Object, hasChanged }) formatOptions?: Intl.NumberFormatOptions
+
 	override getContentTemplate(value: number | undefined, data: TData) {
 		data
-		return html`${this.getNumber(value)?.format() ?? html.nothing}`
+		return html`${this.getNumber(value)?.format(this.formatOptions) ?? html.nothing}`
 	}
 
 	override getEditContentTemplate(value: number | undefined, data: TData) {
@@ -22,7 +25,7 @@ export class DataGridColumnNumber<TData> extends DataGridColumnNumberBase<TData>
 	}
 
 	getSumTemplate(sum: number) {
-		return html`<div ${style({ textAlign: 'center', fontWeight: '500' })}>${sum}</div>`
+		return html`<div ${style({ textAlign: 'center', fontWeight: '500' })}>${sum.format(this.formatOptions)}</div>`
 	}
 }
 
