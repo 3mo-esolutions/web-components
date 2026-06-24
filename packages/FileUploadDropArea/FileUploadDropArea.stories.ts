6c327143d0129a892ea0fb6bff7e0600082bf926
a11delavar <a11delavar@outlook.com>
2026-06-24 18:57:54 +0200

    feat(FileUploadDropArea): Add support for `multiple` file uploads.
    refactor(FileUploadDropArea)!: Rename `fileChange` event to `selectionChange`
    
    refactor(FileUploadDropArea)!: Rename `uploadFile` method to `uploadSelection`

--- packages/FileUploadDropArea/FileUploadDropArea.stories.ts
+++ packages/FileUploadDropArea/FileUploadDropArea.stories.ts
@@ -29,10 +29,39 @@ export const FileUploadDropArea: StoryObj = {
 				user-select: none;
 			}
 		</style>
-		<mo-file-upload-drop-area .upload=${(file: File) => alert(`Upload file ${file.name}`)}>
+		<mo-file-upload-drop-area .upload=${(file: File) => alert(`Upload file "${file.name}"`)}>
 			<mo-flex alignItems='center' gap='0.5rem'>
 				<mo-icon icon='backup'></mo-icon>
 				<span>Click OR Drag & Drop</span>
+			</mo-flex>
+		</mo-file-upload-drop-area>
+	`
+}
+
+export const Multiple: StoryObj = {
+	render: () => html`
+		<style>
+			mo-icon {
+				font-size: 50px;
+				color: var(--mo-color-gray);
+			}
+
+			mo-flex * {
+				transition: 250ms;
+			}
+
+			mo-file-upload-drop-area[drag] * {
+				color: var(--mo-color-accent) !important;
+			}
+
+			span {
+				user-select: none;
+			}
+		</style>
+		<mo-file-upload-drop-area multiple .upload=${(files: Array<File>) => alert(`Upload files ${files.map(file => `"${file.name}"`).formatAsConjunction()}`)}>
+			<mo-flex alignItems='center' gap='0.5rem'>
+				<mo-icon icon='backup'></mo-icon>
+				<span>Click OR Drag & Drop multiple files</span>
 			</mo-flex>
 		</mo-file-upload-drop-area>
 	`
--- packages/FileUploadDropArea/FileUploadDropArea.ts
+++ packages/FileUploadDropArea/FileUploadDropArea.ts
@@ -1,38 +1,40 @@
 import { html, eventListener, css, component, query, Component, property, event, literal, staticHtml, ifDefined } from '@a11d/lit'
-import { type FileUpload } from '@3mo/file-upload'
+import { type FileUpload, type FileUploadSelection } from '@3mo/file-upload'
 
 /**
- * @element mo-file-upload-drop-area UploadDropArea is a component that allows the user to upload a file by dragging it into the component.
+ * @element mo-file-upload-drop-area UploadDropArea is a component that allows the user to upload files by dragging them into the component.
  *
- * @attr upload - The mandatory upload function that is called when the user selects a file.
+ * @attr upload - The mandatory upload function that is called when the user selects one or more files.
+ * @attr multiple - Whether multiple files can be selected at once.
  * @attr accept - The file types that are accepted for upload, specified as a string containing a comma-separated list of MIME types or file extensions.
  *
  * @fires change - Dispatched when the uploading process results in success or failure. The event detail is the result of the upload, either the result of the upload function or undefined if the upload failed.
  * @fires uploadingChange - Dispatched when the uploading process starts or ends. The event detail is true if the uploading process has started, false otherwise.
- * @fires fileChange - Dispatched when the selected file changes. The event detail is the selected file or undefined if no file is selected.
+ * @fires selectionChange - Dispatched when the selection changes. The event detail is the selected file (or array of files when "multiple" is set) or undefined if no file is selected.
  */
 @component('mo-file-upload-drop-area')
-export class FileUploadDropArea<TResult> extends Component {
+export class FileUploadDropArea<TResult, TMultiple extends boolean = false> extends Component {
 	@event() readonly change!: EventDispatcher<TResult | undefined>
 	@event() readonly uploadingChange!: EventDispatcher<boolean>
-	@event() readonly fileChange!: EventDispatcher<File | undefined>
+	@event() readonly selectionChange!: EventDispatcher<FileUploadSelection<TMultiple>>
 
-	@property({ type: Object }) upload!: (file: File) => Promise<TResult>
+	@property({ type: Object }) upload!: (selection: FileUploadSelection<TMultiple>) => Promise<TResult>
+	@property({ type: Boolean }) multiple?: TMultiple | boolean
 	@property() accept?: string
 
-	@query('mo-file-upload') protected readonly uploadElement!: FileUpload<TResult>
+	@query('mo-file-upload') protected readonly uploadElement!: FileUpload<TResult, TMultiple>
 
 	protected readonly fileUploadElementTag = literal`mo-file-upload`
 
-	openExplorer(...parameters: Parameters<FileUpload<TResult>['openExplorer']>) {
+	openExplorer(...parameters: Parameters<FileUpload<TResult, TMultiple>['openExplorer']>) {
 		return this.uploadElement.openExplorer(...parameters)
 	}
 
-	uploadFile(...parameters: Parameters<FileUpload<TResult>['uploadFile']>) {
-		return this.uploadElement.uploadFile(...parameters)
+	uploadSelection(...parameters: Parameters<FileUpload<TResult, TMultiple>['uploadSelection']>) {
+		return this.uploadElement.uploadSelection(...parameters)
 	}
 
-	executeUpload(...parameters: Parameters<FileUpload<TResult>['executeUpload']>) {
+	executeUpload(...parameters: Parameters<FileUpload<TResult, TMultiple>['executeUpload']>) {
 		return this.uploadElement.executeUpload(...parameters)
 	}
 
@@ -64,10 +66,11 @@ export class FileUploadDropArea<TResult> extends Component {
 		return staticHtml`
 			<${this.fileUploadElementTag} uploadOnSelection
 				.upload=${this.upload}
+				?multiple=${this.multiple}
 				accept=${ifDefined(this.accept)}
 				@change=${(e: CustomEvent<TResult | undefined>) => this.change.dispatch(e.detail)}
 				@uploadingChange=${(e: CustomEvent<boolean>) => this.uploadingChange.dispatch(e.detail)}
-				@fileChange=${(e: CustomEvent<File | undefined>) => this.fileChange.dispatch(e.detail)}
+				@selectionChange=${(e: CustomEvent<FileUploadSelection<TMultiple>>) => this.selectionChange.dispatch(e.detail)}
 			></${this.fileUploadElementTag}>
 		`
 	}
@@ -100,13 +103,13 @@ export class FileUploadDropArea<TResult> extends Component {
 
 		this.toggleAttribute('drag', false)
 
-		const file = Array.from(e.dataTransfer?.items ?? [])
+		const files = Array.from(e.dataTransfer?.items ?? [])
 			.filter(item => item.kind === 'file')
 			.map(item => item.getAsFile())
-			.filter(file => !!file)
-			.at(0) as File | undefined
+			.filter(file => !!file) as Array<File>
 
-		await this.uploadFile(file)
+		const selection = this.multiple ? files : files[0]
+		await this.uploadSelection(selection as FileUploadSelection<TMultiple>)
 	}
 }
 
